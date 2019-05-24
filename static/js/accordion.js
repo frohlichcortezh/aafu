9a64992f8f99a0356af21100bccd280df4de9185
frohlichcortezh <frohlichcortezh@gmail.com>
2019-05-24 13:07:05 +0200

    General changes on theme

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -3,19 +3,22 @@ let allPanels = Array.from(document.getElementsByClassName("panel"));
 
 [...acc].forEach(function(elem) {
   if (elem.classList.contains("active")) {
-    let activePanel = elem.nextElementSibling;
-    activePanel.style.maxHeight = activePanel.scrollHeight + "px";
+    setActivePanelHeight(elem);
   }
 });
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    [...acc].forEach(elem => elem.classList.remove("active"));
+    [...acc].forEach(elem => elem.classList.toggle("active", false));
     [...allPanels].forEach(function(elem) {
       elem.style.maxHeight = null;
     });
-    this.classList.add("active");
-    let panel = this.nextElementSibling;
-    panel.style.maxHeight = panel.scrollHeight + "px";
+    this.classList.toggle("active", true);
+    setActivePanelHeight(this);
   });
-}
+};
+
+function setActivePanelHeight(elem) {
+  let activePanel = elem.nextElementSibling;
+  activePanel.style.maxHeight = activePanel.scrollHeight + "px";
+};
