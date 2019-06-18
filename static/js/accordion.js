ee4f041cf4de1698741539f7b074d051300a432f
frohlichcortezh <frohlichcortezh@gmail.com>
2019-06-18 10:08:09 +0200

    Global changes on forked theme
    Adaptation for multi-language
    Added personality and language accordions
    Changed how skills and hobbies are showed

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -2,23 +2,23 @@ let acc = Array.from(document.getElementsByClassName("accordion"));
 let allPanels = Array.from(document.getElementsByClassName("panel"));
 
 [...acc].forEach(function(elem) {
-  if (elem.classList.contains("active")) {
     setActivePanelHeight(elem);
-  }
 });
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    var s = this.classList.contains("active");
     [...acc].forEach(elem => elem.classList.toggle("active", false));
     [...allPanels].forEach(function(elem) {
       elem.style.maxHeight = null;
     });
-    this.classList.toggle("active", true);
-    setActivePanelHeight(this);
+    this.classList.toggle("active", !s);
+      setActivePanelHeight(this);
   });
 };
 
 function setActivePanelHeight(elem) {
-  let activePanel = elem.nextElementSibling;
-  activePanel.style.maxHeight = activePanel.scrollHeight + "px";
+  if (elem.classList.contains("active")) { 
+    elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
+  }
 };
