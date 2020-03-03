91230fde6efcb00184e745bc5b1b2092f61ff3fa
Hector Frohlich Cortez <frohlichcortezh@protonmail.com>
2020-03-03 12:59:07 +0100

    Correction of print.js to open accordion before print

--- static/js/print.js
+++ static/js/print.js
@@ -1,20 +1,6 @@
-var _print = window.print;
-
-window.print = function() {
+window.addEventListener('beforeprint', (event) => {
     [...acc].forEach(function(elem) {
         elem.classList.toggle("active", true);
         elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
-    });
-    var btnPrint = document.getElementById("btnPrint");
-    toogleBtnVisibility(btnPrint);
-    _print();
-    toogleBtnVisibility(btnPrint);
- }
-
-function toogleBtnVisibility(elem) {
-    if (elem.style.display === "none") {
-        elem.style.display = "block";
-    } else {
-        elem.style.display = "none";
-    }
-}
+    });    
+  });
